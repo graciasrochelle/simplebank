cefdfd00d692350af9ea07919c1c75dd77cf2a09
Rochelle Gracias <graciasrochelle@gmail.com>
2024-11-11 17:36:27 +1100

    read constants from config file

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	conn, err := sql.Open(config.DB_DRIVER, config.DB_SOURCE)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.SERVER_ADDRESS)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
